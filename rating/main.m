866452d6696fd0da71d939cb601c71a2ce7c1ac5
Jinah Adam <jinahadam@me.com>
2015-04-10 13:03:19 +0800

    basci

--- rating/main.m
+++ rating/main.m
@@ -10,8 +10,22 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSArray *arguments = [[NSProcessInfo processInfo] arguments];
+        @try {
+            NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://walrusratings.herokuapp.com/%@.json", arguments[1]]]];
+            NSMutableArray *jsonList = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            
+            NSDictionary *jsonDictionary = (NSDictionary *)jsonList;
+            
+            printf("%.2f \n", [[jsonDictionary objectForKey:@"score"] doubleValue]);
+        }
+        @catch (NSException * e) {
+           printf("Symbol not found\n");
+        }
+        @finally {
+        }
+
+
     }
     return 0;
 }
