bd5e0b3728a5ac2e7bf7014411143db5d3552a30
Jinah Adam <jinahadam@me.com>
2015-04-10 12:16:51 +0800

    Initial Commit

---
+++ rating/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  rating
+//
+//  Created by Jinah Adam on 10/4/15.
+//  Copyright (c) 2015 Jinah Adam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
